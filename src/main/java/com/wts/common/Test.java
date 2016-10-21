5ed9f40c025e9dafe03b867c8d778a2d0a04778a
王天硕 <nelsonnick@126.com>
2016-10-21 16:56:19 +0800

    aa

--- src/main/java/com/wts/common/Test.java
+++ src/main/java/com/wts/common/Test.java
@@ -19,7 +19,7 @@ public class Test {
     // 记录info级别的信息
     logger.info("This is info message.");
     // 记录error级别的信息
-    logger.error("This is error message.");
+    logger.error("This is error message.?");
   }
 
 }
